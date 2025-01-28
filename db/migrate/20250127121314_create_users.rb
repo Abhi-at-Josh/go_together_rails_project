789d6cf28972b467564abd111fae0125a73aa1ea
Abhi-at-josh <abhijeet.lokhande@joshsoftware.com>
2025-01-28 11:20:26 +0530

    Generate User model, migrate database, and add validations

---
+++ db/migrate/20250127121314_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_no
+      t.string :email
+      t.string :password
+      t.string :gender
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
