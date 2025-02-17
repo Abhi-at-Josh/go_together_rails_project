76ac8c03ed6178101f8aa31691077ca5f72ed309
Abhi-at-josh <abhijeet.lokhande@joshsoftware.com>
2025-02-17 17:14:11 +0530

    Make the required changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
     private
   
     def user_params
-      params.permit(:first_name, :last_name, :email, :phone_no, :password, :password_confirmation, :gender, :age)
+      params.require(:user).permit(:first_name, :last_name, :email, :phone_no, :password, :password_confirmation, :gender, :age)
     end
   
     def set_user
